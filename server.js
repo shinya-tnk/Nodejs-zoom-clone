122efe7834171da6bc88daddc18f1fac5edbe38e
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-01-11 16:55:37 +0900

    サーバー(ルーティング)の設定

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.get('/', (req, res) => {
+  res.status(200).write("Hello World")
+})
+
+server.listen(3030);
