222b4d8e2d1ab4a765aeaea2102f8bc18cdf91dc
shinya-tnk <70380308+shinya-tnk@users.noreply.github.com>
2021-01-11 17:06:54 +0900

    view engineの設定

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.render('room');
 })
